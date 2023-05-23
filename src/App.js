ed72cc93d113978950c84eb20f0ac67cb87b1bd8
MinseoLee <mmmm@ajou.ac.kr>
2023-05-23 14:19:49 +0900

    setup base layouts

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import * as cv from "@techstark/opencv-js";
+import * as S from './App.module.css';
+import {useEffect, useState} from "react";
 
+
+const processImage = (src) => {
+    cv.imread(src);
+}
+
+// cv.imread("/nklee.jpeg");
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [imgUrl, setImgUrl] = useState(null);
+
+    return (
+        <div className={S["container"]}>
+            <h1 className={'mb-32'}>퍼스널 컬러 진단</h1>
+            <div className={'mb-32'}>
+                <label className={S['input-label']} htmlFor="ex_file">업로드</label>
+                <input
+                    className={S['input-button']}
+                    type="file"
+                    name="file"
+                    id={"ex_file"}
+                    onChange={(e) => {
+                        console.log(e.target.files);
+                        if (e.target.files) {
+                            setImgUrl(() => URL.createObjectURL(e.target.files[0]));
+                        }
+                    }}
+                />
+            </div>
+            <img
+                className={S['img-canvas']}
+                alt="Original input"
+                src={imgUrl}
+                onLoad={(e) => {
+                    processImage(e.target);
+                }}
+            />
+        </div>
+    );
 }
 
 export default App;
